96b60be949994ff6c3b016aef22aa0dbada8c381
hiroki <hiro_bass.0827@icloud.com>
2021-06-14 19:19:45 +0900

    fix

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,31 +1,33 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
-
 void ofApp::setup(){
   ofBackground(0);
-  ofSetFrameRate(30);
+  ofSetFrameRate(FRAME_RATE);
   verdana.load("ofxbraitsch/fonts/Verdana.ttf", 18);
 
   //load and play movies
-  if (faceMovie.load("movies/fizzbuzz_processed.mp4")) faceMovie.play();
-  if (screenMovie.load("movies/fizzbuzz_scratch.mp4")) screenMovie.play();
+  if (face_movie.load(face_movie_path)) face_movie.play();
+  if (screen_movie.load(screen_movie_path)) screen_movie.play();
 
   //settings about ofxDatGui
   ofxDatGuiComponent* component = nullptr;
 
-  movie_duration = faceMovie.getDuration();
+  //slider
+  movie_duration = face_movie.getDuration();
   movie_slider = new ofxDatGuiSlider("", 0.0, movie_duration);
   movie_slider->onSliderEvent(this, &ofApp::onSliderEvent);
   slider_component = movie_slider;
   slider_component->setPosition(400, 340);
   slider_component->setWidth(640, 0);
 
+  //AU_C matrix
   matrix = new ofxDatGuiMatrix("AU_C", AU_NUM, true);
-  matrix->setPosition(200, 560);
+  matrix->setPosition(0, 400);
   matrix->setWidth(200, 60);
   matrix->setRadioMode(true);
 
+  //load csv files
+  /*
   if (of_csv.load("fizzbuzz_processed.csv")) {
     for (size_t i = 0; i < PLOTTER_NUM; ++i) {
       string AU_label = of_csv[0][AU_INDEX_HEAD + i];
@@ -42,48 +44,85 @@ void ofApp::setup(){
       components.push_back(component);
     }
   }
+  */
 
-  for (size_t i = 0; i < AU_NUM; ++i) {
-    string AU_labels = of_csv[0][AU_C_INDEX_HEAD + i];
-    string num = ofToString(AU_labels[3]) + ofToString(AU_labels[4]);
-    int label = stoi(num);
-    matrix->getButtonAtIndex(i)->setMIndex(label-1);
-    matrix->getButtonAtIndex(i)->setSelected(true);
+  //mybeat
+  /*
+  if (mb_csv.load("fizzbuzz_scratch_mybeat.csv")) {
+    for (size_t i = 0; i < MB_PLOTTER_NUM; ++i) {
+      string mb_label;
+      if (i < 2) {
+        mb_label = mb_csv[5][i+9];
+        mb_plotters[i] = new ofxDatGuiValuePlotter(mb_label, 0, 3000);
+      } else {
+        mb_label = "lf/hf";
+        mb_plotters[i] = new ofxDatGuiValuePlotter(mb_label, 0, 5);
+      }
+      mb_plotters[i]->setDrawMode(ofxDatGuiGraph::LINES);
+      mb_plotters[i]->setSpeed(4);
+      component = mb_plotters[i];
+      component->setPosition(0, i * 80);
+      component->setWidth(200, 60);
+      components.push_back(component);
+    }
   }
+  */
+  //openFace
+  if (of_csv.load("reflection_cam.csv")) {
+    for (size_t i = 0; i < AU_NUM; ++i) {
+      string au_labels = of_csv[0][AU_C_INDEX_HEAD + i];
+      string au_num = ofToString(au_labels[3]) + ofToString(au_labels[4]);
+      int label = stoi(au_num);
+      matrix->getButtonAtIndex(i)->setMIndex(label-1);
+      matrix->getButtonAtIndex(i)->setSelected(true);
+    }
+  }
+
 }
 
-//--------------------------------------------------------------
 void ofApp::update(){
+  //time and frame
+  int current_frame = face_movie.getCurrentFrame();
+  int elapsed_movie_sec = current_frame / FRAME_RATE;
 
-  faceMovie.update();
-  screenMovie.update();
+  //movies
+  face_movie.update();
+  screen_movie.update();
 
-  int current_frame = faceMovie.getCurrentFrame();
-  elapsed_movie_time = current_frame / 30;
-
-  if (current_frame > 0 && elapsed_movie_time > 0) {
+  if (current_frame > 0 && elapsed_movie_sec > 0) {
     gaze_0_x = stof(of_csv[current_frame][5]);
     gaze_0_y = stof(of_csv[current_frame][6]);
     gaze_1_x = stof(of_csv[current_frame][8]);
     gaze_1_y = stof(of_csv[current_frame][9]);
 
-    for (size_t i = 0; i < components.size(); ++i) {
-      float current_value = 0.0;
-      current_value = stof(of_csv[current_frame][AU_INDEX_HEAD + i]);
-      plotters[i]->setValue(current_value);
+    //mybeat plotter
+    /*
+    float lf = stof(mb_csv[5+elapsed_movie_sec][9]);
+    float hf = stof(mb_csv[5+elapsed_movie_sec][10]);
+    float lf_hf = lf / hf;
+    for (size_t i = 0; i < MB_PLOTTER_NUM; ++i) {
+      if (i == 0) {
+        mb_plotters[i]->setValue(lf);
+      } else if(i == 1) {
+        mb_plotters[i]->setValue(hf);
+      } else {
+        mb_plotters[i]->setValue(lf_hf);
+      }
       components[i]->update();
     }
+    */
   }
-  
 
-  if (!is_mouse_pressed) movie_slider->setValue(elapsed_movie_time);
+  
+  //slider
+  if (!is_mouse_pressed) movie_slider->setValue(elapsed_movie_sec);
   slider_component->update();
   
+  //AU_C matrix
   if (current_frame > 0) {
     for (int i = AU_C_INDEX_HEAD; i < AU_C_INDEX_END + 1; ++i) {
       int au_c_value = stoi(of_csv[current_frame][i]);
       int matrix_index = i - AU_C_INDEX_HEAD; 
-
       if(stoi(of_csv[current_frame][i])) {
         //cout << "matrix index: " << matrix_index << "\n";
         matrix->getButtonAtIndex(matrix_index-1)->setSelected(false);
@@ -92,20 +131,19 @@ void ofApp::update(){
       }
     }
   }
-
   matrix->update();
+
 }
 
-//--------------------------------------------------------------
 void ofApp::draw(){
   ofSetColor(255);
 
-  faceMovie.draw(400+640, 0, -640, 360);
-  screenMovie.draw(400, 360, 640, 360);
+  face_movie.draw(400+MOVIE_WIDTH, 0, -MOVIE_WIDTH, MOVIE_HEIGHT);
+  screen_movie.draw(400, 360, MOVIE_WIDTH, MOVIE_HEIGHT);
 
   ofSetColor(0, 255, 0);
-  float gaze_x = ofMap(gaze_0_x, -1, 1,  screenMovie.getWidth(), -50); 
-  float gaze_y = ofMap(gaze_0_y, -1, 1, 0, screenMovie.getHeight()); 
+  float gaze_x = ofMap(gaze_0_x, -1, 1,  screen_movie.getWidth(), -50); 
+  float gaze_y = ofMap(gaze_0_y, -1, 1, 0, screen_movie.getHeight()); 
   ofDrawCircle(gaze_x, gaze_y+200, 5);
 
   for (size_t i = 0; i < components.size(); ++i) components[i]->draw();
@@ -117,8 +155,8 @@ void ofApp::draw(){
 //
 void ofApp::onSliderEvent(ofxDatGuiSliderEvent e) {
   if (is_mouse_pressed) {
-    faceMovie.setPosition(e.value / movie_duration);
-    screenMovie.setPosition(e.value / movie_duration);
+    face_movie.setPosition(e.value / movie_duration);
+    screen_movie.setPosition(e.value / movie_duration);
   }
 }
 
@@ -126,61 +164,15 @@ void ofApp::onMatrixEvent(ofxDatGuiMatrixEvent e) {
 
 }
 
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-  //faceMovie.setPosition((float)x / (float)ofGetWidth());
-}
-
-//--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
   is_mouse_pressed = true;
-  faceMovie.setPaused(true);
-  screenMovie.setPaused(true);
+  face_movie.setPaused(true);
+  screen_movie.setPaused(true);
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
   is_mouse_pressed = false;
-  faceMovie.setPaused(false);
-  screenMovie.setPaused(false);
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+  face_movie.setPaused(false);
+  screen_movie.setPaused(false);
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -10,39 +10,38 @@ class ofApp : public ofBaseApp{
 		void update();
 		void draw();
 		
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
 
     void onSliderEvent(ofxDatGuiSliderEvent e);
     void onMatrixEvent(ofxDatGuiMatrixEvent e);
 
-    ofVideoPlayer faceMovie;
-    ofVideoPlayer screenMovie;
+    ofVideoPlayer face_movie;
+    ofVideoPlayer screen_movie;
     ofxCsv of_csv;
-    //ofxCsv mb_csv;
+    ofxCsv mb_csv;
+
+    string face_movie_path = "movies/reflection_cam.mp4";
+    string screen_movie_path = "movies/reflect_screen_trimed.mov";
 
     static const int AU_NUM = 18;
     static const int PLOTTER_NUM = 17;
+    static const int MB_PLOTTER_NUM = 3;
     static const int AU_INDEX_HEAD = 679;
     static const int AU_C_INDEX_HEAD = 696;
     static const int AU_C_INDEX_END = 713;
+    static const int MOVIE_WIDTH = 640;
+    static const int MOVIE_HEIGHT = 400;
+    static const int FRAME_RATE = 30;
+
     float gaze_0_x = 0.0, gaze_0_y = 0.0;
     float gaze_1_x = 0.0, gaze_1_y = 0.0;
-    float elapsed_movie_time = 0.0;
     float movie_duration = 0.0;
     bool is_mouse_pressed = false;
 
     ofTrueTypeFont verdana;
     ofxDatGuiValuePlotter* plotters[PLOTTER_NUM];
+    ofxDatGuiValuePlotter* mb_plotters[MB_PLOTTER_NUM];
     ofxDatGuiSlider* movie_slider;
     ofxDatGuiComponent* slider_component;
     ofxDatGuiMatrix* matrix;
